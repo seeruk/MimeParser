f78471d53f43ca576d69a4884dfa50d95e4897ae
Elliot Wright <wright.elliot@gmail.com>
2013-04-18 12:47:15 +0100

    Directory setup, initial class base.

---
+++ src/SeerUK/Cyclone/Component/MimeParser/Parser/MimeParser.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Cyclone
+ *
+ * (c) Elliot Wright <wright.elliot@gmail.com> - 2013
+ *
+ * For full license information please visit
+ * http://license.visualidiot.com/
+ */
+
+namespace SeerUK\Cyclone\Component\MimeParser\Parser;
+
+/**
+ * Class that parses email messages
+ *
+ * @author  Elliot Wright
+ * @package Cyclone
+ * @version 1.0-prealpha
+ */
+class MimeParser
+{
+
+}
